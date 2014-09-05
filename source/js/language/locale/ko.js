7968b6fc6ac4ad86591070f00ae91e6c20c55597
youngmin <youngmin.k@gmail.com>
2014-09-05 23:51:56 +0900

    ko locale added

---
+++ source/js/language/locale/ko.js
@@ -0,0 +1,15 @@
+VCO.Language = {
+	name: 					"한국어",
+	lang: 					"ko",
+	messages: {
+		loading: 			"지도를 읽어오고 있습니다.",
+		wikipedia: 			"From Wikipedia, the free encyclopedia"
+	},
+	buttons: {
+	    map_overview: 		"전체 보기",
+		overview: 			"개요",
+	    backtostart: 		"처음으로",
+	    collapse_toggle: 	"숨기기",
+	    uncollapse_toggle: 	"보기"
+	}
+}
